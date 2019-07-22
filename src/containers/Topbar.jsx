4e040e1229e088f22f21cbd57a959914fc4f7491
grabeqx <grabowski.kontakt@gmail.com>
2019-07-22 17:33:32 +0200

    Responsive design

--- src/containers/Topbar.jsx
+++ src/containers/Topbar.jsx
@@ -4,6 +4,7 @@ import Logo from "../static/images/logo.png";
 
 const Topbar = (props) => (
   <div className="topbar">
+    <div className="hamburger">&#9776;</div>
     <img src={Logo} alt="logo" className="logo"/>
     <div className="toolbar-contant">
       {props.children}
